020da5e1da73efe42ffaf8acced4e8d89deaf067
hete2333 <1586554415@qq.com>
2026-06-05 19:07:22 +0800

    first commit

---
+++ code2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    //int是整数类型，a和b是两个整数变量
+   byte c,d;
+    //byte是字节类型，c和d是两个字节变量
+    long e,f;
+    //long是长整数类型，e和f是两个长整数变量,本质是长的int类型,所占的内存空间是int类型的2倍
+    long long g,h;
+    //long long是长长整数类型，g和h是两个长长整数变量
+    //long long是长的long类型,所占的内存空间是long类型的2倍
+    float i,j;
+    //float是单精度浮点数类型，i和j是两个单精度浮点数变量,也就是小数
+    double k,l;
+    //double是双精度浮点数类型，k和l是两个双精度浮点数变量,也是小数
+    char m,n;
+    //char是字符类型，m和n是两个字符变量,用来存储单个字符,比如a,b,c等
+    //一些字母，数字，符号等都可以用char类型来存储
+    //以上这些类型都是基本类型，用来存储简单的数据
+    //这些统称变量的类型，或者数据类型，a,b,c,d,e,f,g,h,i,j,k,l,m,n都是变量名，或者标识符，用来标识变量的名字
+    
+    return 0;
+}
